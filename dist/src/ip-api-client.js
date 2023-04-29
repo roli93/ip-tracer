b0d74e58619892434c5feb119cbcf9a2b8f729e9
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2023-04-29 00:06:36 -0300

    Add batch support

--- dist/src/ip-api-client.js
+++ dist/src/ip-api-client.js
@@ -12,13 +12,77 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getIpData = void 0;
+exports.loadIpBatch = exports.getIpData = void 0;
 const axios_1 = __importDefault(require("axios"));
+const remove_1 = __importDefault(require("lodash/remove"));
+const last_1 = __importDefault(require("lodash/last"));
 const httpClient = axios_1.default.create({
-    baseURL: 'http://ip-api.com/json/',
+    baseURL: 'http://ip-api.com/',
 });
+// export const getIpData = async (ip: string) => {
+//     const response = await httpClient.get(`json/${ip}?fields=57539`)
+//     return response.data
+// }
 const getIpData = (ip) => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield httpClient.get(`${ip}?fields=57539`);
-    return response.data;
+    const ipsResult = yield queueIp(ip);
+    return ipsResult.find((r) => r.query === ip);
 });
 exports.getIpData = getIpData;
+const queue = [];
+class IpBatch {
+    constructor(ips) {
+        this.ips = ips;
+        this.resolved = false;
+        this.windowElapsedPromise = this.makeWindowPromise();
+        this.batchFullPromise = this.makeFullPromise();
+        this.resultPromise = Promise.race([this.windowElapsedPromise, this.batchFullPromise]);
+    }
+    isFull() {
+        return this.ips.length === 2;
+    }
+    addIp(ip) {
+        this.ips.push(ip);
+        if (this.isFull()) {
+            this.onFull && this.onFull();
+        }
+    }
+    makeWindowPromise() {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                if (!this.resolved) {
+                    (0, exports.loadIpBatch)(this.ips).then(res).catch(rej).finally(this.resolve.bind(this));
+                }
+            }, 10000);
+        });
+    }
+    makeFullPromise() {
+        return new Promise((res, rej) => {
+            this.onFull = () => {
+                if (!this.resolved) {
+                    (0, exports.loadIpBatch)(this.ips).then(res).catch(rej).finally(this.resolve.bind(this));
+                }
+            };
+        });
+    }
+    resolve() {
+        this.resolved = true;
+        (0, remove_1.default)(queue, b => b === this);
+    }
+}
+const queueIp = (ip) => {
+    const currentBatch = (0, last_1.default)(queue);
+    if (currentBatch && !currentBatch.isFull()) {
+        currentBatch.addIp(ip);
+        return currentBatch.resultPromise;
+    }
+    else {
+        const newBatch = new IpBatch([ip]);
+        queue.push(newBatch);
+        return newBatch.resultPromise;
+    }
+};
+const loadIpBatch = (ips) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield httpClient.post(`batch?fields=61439`, ips);
+    return response.data;
+});
+exports.loadIpBatch = loadIpBatch;
--- src/ip-api-client.ts
+++ src/ip-api-client.ts
@@ -1,10 +1,94 @@
 import axios from 'axios';
+import isEmpty from 'lodash/isEmpty'
+import remove from 'lodash/remove'
+import last from 'lodash/last'
 
 const httpClient = axios.create({
-    baseURL: 'http://ip-api.com/json/',
+    baseURL: 'http://ip-api.com/',
 });
 
+// export const getIpData = async (ip: string) => {
+//     const response = await httpClient.get(`json/${ip}?fields=57539`)
+//     return response.data
+// }
+
 export const getIpData = async (ip: string) => {
-    const response = await httpClient.get(`${ip}?fields=57539`)
+    const ipsResult = await queueIp(ip);
+    return ipsResult.find((r: { query: string; }) => r.query === ip)
+}
+
+
+const queue: IpBatch[] = [];
+
+
+class IpBatch {
+    ips: string[];
+    resolved: boolean;
+    resultPromise: Promise<any>;
+    private windowElapsedPromise: Promise<any>;
+    private batchFullPromise: Promise<any>;
+    private onFull?: () => any;
+
+    constructor(ips: string[]) {
+        this.ips = ips;
+        this.resolved = false;
+        this.windowElapsedPromise = this.makeWindowPromise()
+        this.batchFullPromise = this.makeFullPromise()
+        this.resultPromise = Promise.race([this.windowElapsedPromise, this.batchFullPromise])
+    }
+
+    isFull(){
+        return this.ips.length === 2;
+    }
+
+    addIp(ip: string){
+        this.ips.push(ip);
+        if(this.isFull()){
+            this.onFull && this.onFull()
+        }
+    }
+
+    makeWindowPromise(){
+        return new Promise((res, rej) =>{
+            setTimeout(() => {
+                if(!this.resolved){
+                    loadIpBatch(this.ips).then(res).catch(rej).finally(this.resolve.bind(this))
+                }
+            },10000)
+        })
+    }
+
+    makeFullPromise(){
+        return new Promise((res, rej) =>{
+            this.onFull = () => {
+                if(!this.resolved){
+                    loadIpBatch(this.ips).then(res).catch(rej).finally(this.resolve.bind(this))
+                }
+            }
+        })
+    }
+
+    resolve(){
+        this.resolved = true;
+        remove(queue, b => b === this)
+    }
+
+}
+
+const queueIp = (ip: string) => {
+    const currentBatch = last(queue);
+    if(currentBatch && !currentBatch.isFull()){
+        currentBatch.addIp(ip)
+        return currentBatch.resultPromise;
+    } else {
+        const newBatch = new IpBatch([ip]);
+        queue.push(newBatch)
+        return newBatch.resultPromise;
+    }
+}
+
+
+export const loadIpBatch = async (ips: string[]) => {
+    const response = await httpClient.post(`batch?fields=61439`, ips)
     return response.data
 }
